30c8e1a22d1f3ae81c3ceb303a6ffc7b63648501
franciscorredor <francisco.corredor.pardo@gmail.com>
2017-03-16 06:25:41 -0500

    todo

--- PokerKnowledgeBase/src/com/wirelesssoft/play/Hand.java
+++ PokerKnowledgeBase/src/com/wirelesssoft/play/Hand.java
@@ -21,6 +21,40 @@ public class Hand {
 			System.out.println(pokerCard.toString() );
 		}
 
+//***********************************************************        
+        Hacer evaluacion para 5 jugadores
+        
+        son 5*2 = 10 cartas mas + 5 cartas que estan sobre la mesa: 15 cartas.
+        
+        Se deben mostrar las 15 cartas jugadas, para verificar que no se repitan
+        
+        y mostrar los 5 juegos>
+        
+        Sample:
+			cta1privada		cta2privada		cta3publica		cta4publica		cta5publica		cta6publica		cta7publica		Resultado
+Partida	1 (5Jugadores, solo hay se muestran dos jugadores porque los otros sesisstieron [ref:pokerist.com])	
+		j1:		7T				QD				QC				10P				3C				8C				9C				LOST	, 1par
+		j2:		QP				8P				QC				10P				3C				8C				9C				WIN		, 2pares
+Partida	2 (5Jugadores, solo muestra dos jugadores EL MIO Y EL QUE GANO LA PARTIDA [ref:pokerist.com])	
+		j1:		5T				7T				3D				8T				3P				5P				2C				LOST	, 2 PARES DE MENOR VALOR
+		j2:		QT				QD				3D				8T				3P				5P				2C				WIN		, 2 PARES
+Partida	3 (5Jugadores, solo muestra dos jugadores EL MIO Y EL QUE GANO LA PARTIDA [ref:pokerist.com])	
+		j1:		8C				AD				7T				KT				5P				2P				9D				LOST	, NADA
+		j2:		KC				9T				7T				KT				5P				2P				9D				WIN		, 2 PARES
+Partida	4 (5Jugadores, solo muestra MI JUEGO, LOS DEMAS DESISTIERON [ref:pokerist.com])	
+		j1:		3T				AP				QP				8D				2C				8T				6D				WIN		, 1 PAR Q TUBIERON TODOS, MI CARTA GANADORA FUE EL AS DE PICAS
+
+		T:Trebol
+		P:Picas
+		C:Corazon
+		D:Diamante
+		
+//***********************************************************	
+		
+		Verificar la evaluaci[on de las jugadas...
+		                      
+//***********************************************************
+
         int[] ranks = new int[14];
         //miscellaneous cards that are not otherwise significant
         int[] orderedRanks = new int[5];
