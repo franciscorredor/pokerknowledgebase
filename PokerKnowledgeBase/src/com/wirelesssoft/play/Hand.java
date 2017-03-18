13e2bcd70f71f555b0e9911ebb303af1eb1a6731
Francisco <francisco.corredor.pardo@gmail.com>
2017-03-18 09:27:03 -0500

    actualiza Hand

--- PokerKnowledgeBase/src/com/wirelesssoft/play/Hand.java
+++ PokerKnowledgeBase/src/com/wirelesssoft/play/Hand.java
@@ -5,221 +5,127 @@ import com.wirelesssoft.object.PokerCard;
 
 public class Hand {
 
-    private PokerCard[] cards;
+    private PokerCard[] cardsGamer1;
+    private PokerCard[] cardsGamer2;
+    private PokerCard[] cardsGamer3;
+    private PokerCard[] cardsGamer4;
+    private PokerCard[] cardsGamer5;
+    private PokerCard[] tableGame;
     private int[] value;
 
     public Hand(Deck d)
     {
         value = new int[6];
-        cards = new PokerCard[5];
+        cardsGamer1 = new PokerCard[2];
+        cardsGamer2 = new PokerCard[2];
+        cardsGamer3 = new PokerCard[2];
+        cardsGamer4 = new PokerCard[2];
+        cardsGamer5 = new PokerCard[2];
+        tableGame = new PokerCard[5];
+        
+        for (int x=0; x<2; x++)
+        {
+        	cardsGamer1[x] = d.drawFromDeck();
+        }
+        for (int x=0; x<2; x++)
+        {
+        	cardsGamer2[x] = d.drawFromDeck();
+        }
+        for (int x=0; x<2; x++)
+        {
+        	cardsGamer3[x] = d.drawFromDeck();
+        }
+        for (int x=0; x<2; x++)
+        {
+        	cardsGamer4[x] = d.drawFromDeck();
+        }
+        for (int x=0; x<2; x++)
+        {
+        	cardsGamer5[x] = d.drawFromDeck();
+        }
         for (int x=0; x<5; x++)
         {
-            cards[x] = d.drawFromDeck();
+        	tableGame[x] = d.drawFromDeck();
         }
         
-        for (PokerCard pokerCard : cards) {
-			System.out.println(pokerCard.toString() );
+        System.out.print("g1 : [" + cardsGamer1[0] + "] [" +  cardsGamer1[1] + "]" );
+        for (PokerCard pokerCard : tableGame) {
+			System.out.print(" [" + pokerCard.toString() + "]" );
 		}
+        System.out.println("");
+        System.out.print("g2 : [" + cardsGamer2[0] + "] [" +  cardsGamer2[1] + "]" );
+        for (PokerCard pokerCard : tableGame) {
+			System.out.print(" [" + pokerCard.toString() + "]" );
+		}
+        System.out.println("");
+        System.out.print("g3 : [" + cardsGamer3[0] + "] [" +  cardsGamer3[1] + "]" );
+        for (PokerCard pokerCard : tableGame) {
+			System.out.print(" [" + pokerCard.toString() + "]" );
+		}
+        System.out.println("");
+        System.out.print("g4 : [" + cardsGamer4[0] + "] [" +  cardsGamer4[1] + "]" );
+        for (PokerCard pokerCard : tableGame) {
+			System.out.print(" [" + pokerCard.toString() + "]" );
+		}
+        System.out.println("");
+        System.out.print("g5 : [" + cardsGamer5[0] + "] [" +  cardsGamer5[1] + "]" );
+        for (PokerCard pokerCard : tableGame) {
+			System.out.print(" [" + pokerCard.toString() + "]" );
+		}
+        System.out.println("");
+        
+        
+        /*
+        for (PokerCard pokerCard : cardsGamer1) {
+			System.out.println("g1" + pokerCard.toString() );
+		}
+        for (PokerCard pokerCard : cardsGamer2) {
+			System.out.println("g2" + pokerCard.toString() );
+		}
+        for (PokerCard pokerCard : cardsGamer3) {
+			System.out.println("g3" + pokerCard.toString() );
+		}
+        for (PokerCard pokerCard : cardsGamer4) {
+			System.out.println("g4" + pokerCard.toString() );
+		}
+        for (PokerCard pokerCard : cardsGamer5) {
+			System.out.println("g5" + pokerCard.toString() );
+		}
+		*/
 
 //***********************************************************        
-        Hacer evaluacion para 5 jugadores
-        
-        son 5*2 = 10 cartas mas + 5 cartas que estan sobre la mesa: 15 cartas.
-        
-        Se deben mostrar las 15 cartas jugadas, para verificar que no se repitan
-        
-        y mostrar los 5 juegos>
-        
-        Sample:
-			cta1privada		cta2privada		cta3publica		cta4publica		cta5publica		cta6publica		cta7publica		Resultado
-Partida	1 (5Jugadores, solo hay se muestran dos jugadores porque los otros sesisstieron [ref:pokerist.com])	
-		j1:		7T				QD				QC				10P				3C				8C				9C				LOST	, 1par
-		j2:		QP				8P				QC				10P				3C				8C				9C				WIN		, 2pares
-Partida	2 (5Jugadores, solo muestra dos jugadores EL MIO Y EL QUE GANO LA PARTIDA [ref:pokerist.com])	
-		j1:		5T				7T				3D				8T				3P				5P				2C				LOST	, 2 PARES DE MENOR VALOR
-		j2:		QT				QD				3D				8T				3P				5P				2C				WIN		, 2 PARES
-Partida	3 (5Jugadores, solo muestra dos jugadores EL MIO Y EL QUE GANO LA PARTIDA [ref:pokerist.com])	
-		j1:		8C				AD				7T				KT				5P				2P				9D				LOST	, NADA
-		j2:		KC				9T				7T				KT				5P				2P				9D				WIN		, 2 PARES
-Partida	4 (5Jugadores, solo muestra MI JUEGO, LOS DEMAS DESISTIERON [ref:pokerist.com])	
-		j1:		3T				AP				QP				8D				2C				8T				6D				WIN		, 1 PAR Q TUBIERON TODOS, MI CARTA GANADORA FUE EL AS DE PICAS
-
-		T:Trebol
-		P:Picas
-		C:Corazon
-		D:Diamante
+//        Hacer evaluacion para 5 jugadores
+//        
+//        son 5*2 = 10 cartas mas + 5 cartas que estan sobre la mesa: 15 cartas.
+//        
+//        Se deben mostrar las 15 cartas jugadas, para verificar que no se repitan
+//        
+//        y mostrar los 5 juegos>
+//        
+//        Sample:
+//			cta1privada		cta2privada		cta3publica		cta4publica		cta5publica		cta6publica		cta7publica		Resultado
+//Partida	1 (5Jugadores, solo hay se muestran dos jugadores porque los otros sesisstieron [ref:pokerist.com])	
+//		j1:		7T				QD				QC				10P				3C				8C				9C				LOST	, 1par
+//		j2:		QP				8P				QC				10P				3C				8C				9C				WIN		, 2pares
+//Partida	2 (5Jugadores, solo muestra dos jugadores EL MIO Y EL QUE GANO LA PARTIDA [ref:pokerist.com])	
+//		j1:		5T				7T				3D				8T				3P				5P				2C				LOST	, 2 PARES DE MENOR VALOR
+//		j2:		QT				QD				3D				8T				3P				5P				2C				WIN		, 2 PARES
+//Partida	3 (5Jugadores, solo muestra dos jugadores EL MIO Y EL QUE GANO LA PARTIDA [ref:pokerist.com])	
+//		j1:		8C				AD				7T				KT				5P				2P				9D				LOST	, NADA
+//		j2:		KC				9T				7T				KT				5P				2P				9D				WIN		, 2 PARES
+//Partida	4 (5Jugadores, solo muestra MI JUEGO, LOS DEMAS DESISTIERON [ref:pokerist.com])	
+//		j1:		3T				AP				QP				8D				2C				8T				6D				WIN		, 1 PAR Q TUBIERON TODOS, MI CARTA GANADORA FUE EL AS DE PICAS
+//
+//		T:Trebol
+//		P:Picas
+//		C:Corazon
+//		D:Diamante
 		
 //***********************************************************	
 		
-		Verificar la evaluaci[on de las jugadas...
+//		Verificar la evaluaci[on de las jugadas...
 		                      
 //***********************************************************
-
-        int[] ranks = new int[14];
-        //miscellaneous cards that are not otherwise significant
-        int[] orderedRanks = new int[5];
-        boolean flush=true, straight=false;
-        int sameCards=1,sameCards2=1;
-        int largeGroupRank=0,smallGroupRank=0;
-        int index=0;
-        int topStraightValue=0;
-
-        for (int x=0; x<=13; x++)
-        {
-            ranks[x]=0;
-        }
-        for (int x=0; x<=4; x++)
-        {
-            ranks[ cards[x].getRank() ]++;
-        }
-        for (int x=0; x<4; x++) {
-            if ( cards[x].getSuit() != cards[x+1].getSuit() )
-                flush=false;
-        }
-
-        for (int x=13; x>=1; x--)
-        {
-                 if (ranks[x] > sameCards)
-                 {
-                     if (sameCards != 1)
-                     //if sameCards was not the default value
-                     {
-                         sameCards2 = sameCards;
-                         smallGroupRank = largeGroupRank;
-                     }
-
-                     sameCards = ranks[x];
-                     largeGroupRank = x;
-
-                 } else if (ranks[x] > sameCards2)
-                 {
-                     sameCards2 = ranks[x];
-                     smallGroupRank = x;
-                 }
-        }
-
-        if (ranks[1]==1) //if ace, run this before because ace is highest card
-        {
-            orderedRanks[index]=14;
-            index++;
-        }
-
-        for (int x=13; x>=2; x--)
-        {
-            if (ranks[x]==1)
-            {
-                orderedRanks[index]=x; //if ace
-                index++;
-            }
-        }
-        
-        for (int x=1; x<=9; x++)
-        //can't have straight with lowest value of more than 10
-        {
-            if (ranks[x]==1 && ranks[x+1]==1 && ranks[x+2]==1 && 
-                ranks[x+3]==1 && ranks[x+4]==1)
-            {
-                straight=true;
-                topStraightValue=x+4; //4 above bottom value
-                break;
-            }
-        }
-
-        if (ranks[10]==1 && ranks[11]==1 && ranks[12]==1 && 
-            ranks[13]==1 && ranks[1]==1) //ace high
-        {
-            straight=true;
-            topStraightValue=14; //higher than king
-        }
-        
-        if (ranks[10]==1 && ranks[11]==1 && ranks[12]==1 && 
-                ranks[13]==1 && ranks[1]==1) //ace high
-            {
-                straight=true;
-                topStraightValue=14; //higher than king
-            }
-
-       for (int x=0; x<=5; x++)
-            {
-                value[x]=0;
-            }
-       
-
-       //start hand evaluation
-         if ( sameCards==1 ) {    //if we have no pair...
-             value[0]=1;          //this is the lowest type of hand, so it gets the lowest value
-             value[1]=orderedRanks[0];  //the first determining factor is the highest card,
-             value[2]=orderedRanks[1];  //then the next highest card,
-             value[3]=orderedRanks[2];  //and so on
-             value[4]=orderedRanks[3];
-             value[5]=orderedRanks[4];
-         }
-
-         if (sameCards==2 && sameCards2==1) //if 1 pair
-         {
-             value[0]=2;                //pair ranked higher than high card
-             value[1]=largeGroupRank;   //rank of pair
-             value[2]=orderedRanks[0];  //next highest cards.
-             value[3]=orderedRanks[1];
-             value[4]=orderedRanks[2];
-         }
-
-         if (sameCards==2 && sameCards2==2) //two pair
-         {
-             value[0]=3;
-             //rank of greater pair
-             value[1]= largeGroupRank>smallGroupRank ? largeGroupRank : smallGroupRank;
-             //rank of smaller pair
-             value[2]= largeGroupRank<smallGroupRank ? largeGroupRank : smallGroupRank;
-             value[3]=orderedRanks[0];  //extra card
-         }
-
-         if (sameCards==3 && sameCards2!=2)
-         //three of a kind (not full house)
-         {
-             value[0]=4;
-             value[1]= largeGroupRank;
-             value[2]=orderedRanks[0];
-             value[3]=orderedRanks[1];
-         }
-
-         if (straight)
-         {
-             value[0]=5;
-             value[1]=5;
-             //if we have two straights, 
-             //the one with the highest top cards wins
-         }
-
-         if (flush)   
-         {
-             value[0]=6;
-             value[1]=orderedRanks[0]; //tie determined by ranks of cards
-             value[2]=orderedRanks[1];
-             value[3]=orderedRanks[2];
-             value[4]=orderedRanks[3];
-             value[5]=orderedRanks[4];
-         }
-
-         if (sameCards==3 && sameCards2==2)  //full house
-         {
-             value[0]=7;
-             value[1]=largeGroupRank;
-             value[2]=smallGroupRank;
-         }
-
-         if (sameCards==4)  //four of a kind
-         {
-             value[0]=8;
-             value[1]=largeGroupRank;
-             value[2]=orderedRanks[0];
-         }
-
-         if (straight && flush)  //straight flush
-         {
-             value[0]=9;
-             value[1]=9;
-         }
         
         
     }
@@ -271,11 +177,7 @@ Partida	4 (5Jugadores, solo muestra MI JUEGO, LOS DEMAS DESISTIERON [ref:pokeris
         System.out.println(s);
     }
 
-    void displayAll()
-    {
-        for (int x=0; x<5; x++)
-            System.out.println(cards[x]);
-    }
+   
 
     int compareTo(Hand that)
     {
